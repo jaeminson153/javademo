75b29ceb6b81c7531c3660eae364bf9c54f560ab
sonjaemin <jaeminson153@gmail.com>
2025-05-26 17:01:22 +0900

    20250526 add ncs12

---
+++ src/ncs/test12/Fruit.java
@@ -0,0 +1,49 @@
+package ncs.test12;
+
+public class Fruit {
+	private String name;
+	private int price;
+	private int quantity;
+	 
+	public Fruit() {
+	
+	}
+	
+	public Fruit(String name, int price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public String toString() {
+		return String.format("%s, %4d원, %2d개",name, price, quantity);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+	
+}
