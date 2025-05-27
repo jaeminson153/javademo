280b00218bdb182153b38f0332cce981ca9fbf7b
sonjaemin <jaeminson153@gmail.com>
2025-05-27 17:41:01 +0900

    20250227 add ncs 13

---
+++ src/ncs/test13/AmountNotEnough.java
@@ -0,0 +1,11 @@
+package ncs.test13;
+
+public class AmountNotEnough extends Exception{
+	
+	public AmountNotEnough() {
+		super();
+	}
+	public AmountNotEnough(String message) {
+		super(message);
+	}
+}
--- src/ncs/test13/Inventory.java
+++ src/ncs/test13/Inventory.java
@@ -1,6 +1,8 @@
 package ncs.test13;
 
-import java.sql.Date;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Inventory {
 	private String productName;   	//상품명 
@@ -14,14 +16,29 @@ public class Inventory {
 	
 	}
 	
-	public Inventory(String productName, Date putDate, int putAmount) {
-		this.productName = productName;
-		this.putDate = putDate;
-		this.putAmount = putAmount;		
+	public Inventory(String productName, String putDate, int putAmount) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy.mm.dd");
+		Date date;
+		try {
+			date = sdf.parse(putDate);
+			this.productName = productName;
+			this.putDate = date;
+			this.putAmount = putAmount;		
+			this.getAmount = 0;
+			this.getDate = null;
+			this.inventoryAmount = putAmount;
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}		
+
 	}
 
 	public String toString() {
-		return null;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 mm월 dd일");
+		
+		return String.format("%-12s\t,%13s 입고, %d개, %s, %d개, 재고 %d개", productName, putDate != null ? sdf.format(putDate) : "null", putAmount, getDate != null ? sdf.format(getDate) : "null",  getAmount, inventoryAmount);
+		//return String.format("%12s, %s 입고, %d개",productName,sdf.format(putDate), putAmount);
 	}
 	
 	public String getProductName() {
@@ -54,14 +71,19 @@ public class Inventory {
 
 	public void setPutAmount(int putAmount) {
 		this.putAmount = putAmount;
+		this.inventoryAmount = this.putAmount - this.getAmount;
 	}
 
 	public int getGetAmount() {
 		return getAmount;
 	}
 
-	public void setGetAmount(int getAmount) {
+	public void setGetAmount(int getAmount) throws AmountNotEnough {
 		this.getAmount = getAmount;
+		if(this.putAmount - this.getAmount < 0 ) 			
+			throw new AmountNotEnough("현재 재고가 부족합니다. 재고수량 확인하시기 바랍니다.");
+		else
+			this.inventoryAmount = this.putAmount - this.getAmount;
 	}
 
 	public int getInventoryAmount() {
